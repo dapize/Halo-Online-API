23af64fdd91822bb31d9a0e55508e7de163fe4cd
dapize <danielpz@outlook.com>
2022-05-20 05:26:30 -0500

    :art: CSP Configs and little changes in the index.ts

--- src/index.ts
+++ src/index.ts
@@ -38,16 +38,30 @@ const http = express();
 http.get('*', (req, res) => {
   res.redirect('https://' + req.headers.host + req.url);
 })
-http.use(helmet({ contentSecurityPolicy: false }));
 http.use(compression({ filter: shouldCompress }))
 http.use(redirectWwwTraffic);
 http.listen(80, () => {
-  console.log('Init server without SLL');
+  console.log('Init server without SSL, por 80');
 });
 
 // Init Server
 const app = express();
-app.use(helmet());
+app.use(helmet({
+  contentSecurityPolicy: {
+    useDefaults: true,
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://tagmanager.google.com", "https://www.googletagmanager.com", "https://www.google-analytics.com", "'sha256-IFLwGasMSK0CkL5uDNotHgL1Px0uJRP8gyX7sY0IfHU='"],
+      imgSrc: ["'self'", "https://ssl.gstatic.com",  "https://www.gstatic.com", "https://i.ytimg.com"],
+      frameSrc: ["'self'", "https://www.youtube.com", "https://www.youtube.com/embed/"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com", "https://fonts.googleapis.com"],
+      connectSrc: ["'self'", "https://www.google-analytics.com"]
+    }
+  },
+  crossOriginResourcePolicy: false,
+  crossOriginEmbedderPolicy: false
+}));
+
 app.use(cors({ origin: process.env.CORS_ORIGIN?.split(',') }));
 app.use(express.json());
 const accessLogStream = createStream('access.log', {
@@ -57,13 +71,13 @@ const accessLogStream = createStream('access.log', {
 app.use(morgan('combined', { stream: accessLogStream }))
 app.use(compression({ filter: shouldCompress }))
 app.use(redirectWwwTraffic);
-app.use( routerFiles );
-app.use( routerHardware );
+app.use(routerFiles);
+app.use(routerHardware);
 app.use('/', express.static('public'));
 
 const httpsServer = https.createServer({
-  ca: fs.readFileSync(__dirname + "/ssl/ca_bundle.crt", 'utf8'),
-  key: fs.readFileSync(__dirname + "/ssl/private.key", 'utf8'),
-  cert: fs.readFileSync(__dirname + "/ssl/certificate.crt", 'utf8')
+  ca: fs.readFileSync(__dirname + "/../ssl/ca_bundle.crt", 'utf8'),
+  key: fs.readFileSync(__dirname + "/../ssl/private.key", 'utf8'),
+  cert: fs.readFileSync(__dirname + "/../ssl/certificate.crt", 'utf8')
 }, app);
-httpsServer.listen(443, () => console.log('Init server with SSL'));
+httpsServer.listen(443, () => console.log('Init server with SSL, port 443'));
